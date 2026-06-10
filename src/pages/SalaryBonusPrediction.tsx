6be497bad4cdb3bd7e50e538c4eeefcc43b7bf5f
son nguyen <sonnhfit@gmail.com>
2026-06-10 11:22:07 +0700

    udapte

---
+++ src/pages/SalaryBonusPrediction.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SalaryManagement from './SalaryManagement';
+
+const SalaryBonusPrediction: React.FC = () => {
+  return <SalaryManagement defaultTab="bonus" lockTab />;
+};
+
+export default SalaryBonusPrediction;
